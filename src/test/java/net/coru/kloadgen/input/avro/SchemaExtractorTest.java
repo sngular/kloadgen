69e3af91fbc4f91acd872e7b06fa3aace5db2c69
Jose Enrique Garcia Macineiras <joseegman@ext.inditex.com>
2020-03-27 01:02:15 +0100

    Fixing Test.

--- src/test/java/net/coru/kloadgen/input/avro/SchemaExtractorTest.java
+++ src/test/java/net/coru/kloadgen/input/avro/SchemaExtractorTest.java
@@ -1,14 +1,22 @@
 package net.coru.kloadgen.input.avro;
 
-import static net.coru.kloadgen.util.SchemaRegistryKeys.*;
+import static net.coru.kloadgen.util.SchemaRegistryKeys.SCHEMA_REGISTRY_PASSWORD_KEY;
+import static net.coru.kloadgen.util.SchemaRegistryKeys.SCHEMA_REGISTRY_URL;
+import static net.coru.kloadgen.util.SchemaRegistryKeys.SCHEMA_REGISTRY_USERNAME_KEY;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 import net.coru.kloadgen.model.FieldValueMapping;
+import org.apache.jmeter.threads.JMeterContext;
 import org.apache.jmeter.threads.JMeterContextService;
+import org.apache.jmeter.threads.JMeterVariables;
+import org.apache.jmeter.util.JMeterUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import ru.lanwen.wiremock.ext.WiremockResolver;
@@ -22,6 +30,16 @@ import ru.lanwen.wiremock.ext.WiremockUriResolver;
 class SchemaExtractorTest {
 
   private SchemaExtractor schemaExtractor = new SchemaExtractor();
+
+  @BeforeEach
+  public void setUp() {
+    File file = new File("src/test/resources");
+    String absolutePath = file.getAbsolutePath();
+    JMeterUtils.loadJMeterProperties(absolutePath + "/kloadgen.properties");
+    JMeterContext jmcx = JMeterContextService.getContext();
+    jmcx.setVariables(new JMeterVariables());
+    JMeterUtils.setLocale(Locale.ENGLISH);
+  }
 
   @Test
   public void testFlatPropertiesListSimpleRecord(@Wiremock WireMockServer server) throws IOException, RestClientException {
