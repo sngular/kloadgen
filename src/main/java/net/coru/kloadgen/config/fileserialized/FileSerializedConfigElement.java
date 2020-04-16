4fb2eb6fe14756c92a98784bd3318066cf1d046c
Diego Fuentes Fernández <diegoffern@ext.zara.com>
2020-04-16 18:30:00 +0200

    [https://github.com/corunet/kloadgen/issues/9] add test editor

---
+++ src/main/java/net/coru/kloadgen/config/fileserialized/FileSerializedConfigElement.java
@@ -0,0 +1,49 @@
+package net.coru.kloadgen.config.fileserialized;
+
+import static net.coru.kloadgen.util.ProducerKeysHelper.SAMPLE_ENTITY;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import net.coru.kloadgen.loadgen.BaseLoadGenerator;
+import net.coru.kloadgen.loadgen.impl.AvroLoadGenerator;
+import net.coru.kloadgen.model.FieldValueMapping;
+import org.apache.jmeter.config.ConfigTestElement;
+import org.apache.jmeter.engine.event.LoopIterationEvent;
+import org.apache.jmeter.engine.event.LoopIterationListener;
+import org.apache.jmeter.testbeans.TestBean;
+import org.apache.jmeter.threads.JMeterContextService;
+import org.apache.jmeter.threads.JMeterVariables;
+
+@Getter
+@Setter
+@Slf4j
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileSerializedConfigElement  extends ConfigTestElement implements TestBean, LoopIterationListener {
+
+  private String avroSubject;
+
+  private List<FieldValueMapping> schemaProperties;
+
+  private BaseLoadGenerator generator;
+
+  @Override
+  public void iterationStart(LoopIterationEvent loopIterationEvent) {
+
+    try {
+      if (generator ==  null) {
+        generator = new AvroLoadGenerator(avroSubject, schemaProperties);
+      }
+
+      JMeterVariables variables = JMeterContextService.getContext().getVariables();
+      variables.putObject(SAMPLE_ENTITY, generator.nextMessage());
+    } catch (Exception e) {
+      log.error("Failed to create AvroLoadGenerator instance", e);
+    }
+  }
+
+}
---
+++ src/main/java/net/coru/kloadgen/input/avro/FileSubjectPropertyEditor.java
@@ -0,0 +1,190 @@
+package net.coru.kloadgen.input.avro;
+
+import static net.coru.kloadgen.util.SchemaRegistryKeyHelper.SCHEMA_REGISTRY_SUBJECTS;
+
+import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.beans.PropertyDescriptor;
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorSupport;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileSystemView;
+import lombok.extern.slf4j.Slf4j;
+import net.coru.kloadgen.model.FieldValueMapping;
+import net.coru.kloadgen.util.PropsKeysHelper;
+import org.apache.jmeter.gui.ClearGui;
+import org.apache.jmeter.gui.GuiPackage;
+import org.apache.jmeter.testbeans.gui.GenericTestBeanCustomizer;
+import org.apache.jmeter.testbeans.gui.TableEditor;
+import org.apache.jmeter.testbeans.gui.TestBeanGUI;
+import org.apache.jmeter.testbeans.gui.TestBeanPropertyEditor;
+import org.apache.jmeter.threads.JMeterContextService;
+
+@Slf4j
+public class FileSubjectPropertyEditor  extends PropertyEditorSupport implements ActionListener, TestBeanPropertyEditor, ClearGui {
+
+  private JComboBox<String> subjectNameComboBox;
+
+  private JButton loadClassBtn = new JButton("Loadinin Subject");
+
+  private JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+
+  private JPanel panel = new JPanel();
+
+  private PropertyDescriptor propertyDescriptor;
+
+  private SchemaExtractor schemaExtractor = new SchemaExtractor();
+
+  public FileSubjectPropertyEditor() {
+    this.init();
+  }
+
+  public FileSubjectPropertyEditor(Object source) {
+    super(source);
+    this.init();
+    this.setValue(source);
+  }
+
+  public FileSubjectPropertyEditor(PropertyDescriptor propertyDescriptor) {
+    super(propertyDescriptor);
+    this.propertyDescriptor = propertyDescriptor;
+    this.init();
+  }
+
+  private void init() {
+    subjectNameComboBox = new JComboBox<>();
+    panel.setLayout(new BorderLayout());
+    panel.add(subjectNameComboBox);
+    panel.add(fileChooser);
+    panel.add(loadClassBtn, BorderLayout.AFTER_LINE_ENDS);
+    this.loadClassBtn.addActionListener(this);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent event) {
+    String content = null;
+    try {
+      content = new String(Files.readAllBytes(Paths.get(this.fileChooser.getSelectedFile().getPath())));
+    } catch (IOException e){
+      JOptionPane.showMessageDialog(null, "Failed retrieve schema properties : " + e.getMessage(), "ERROR: Failed to retrieve properties!",
+          JOptionPane.ERROR_MESSAGE);
+      log.error(e.getMessage(), e);
+    }
+    String subjectName = Objects.requireNonNull(content);
+
+    try {
+      List<FieldValueMapping> attributeList = schemaExtractor.flatPropertiesList(subjectName);
+
+      //Get current test GUI component
+      TestBeanGUI testBeanGUI = (TestBeanGUI) GuiPackage.getInstance().getCurrentGui();
+      Field customizer = TestBeanGUI.class.getDeclaredField(PropsKeysHelper.CUSTOMIZER);
+      customizer.setAccessible(true);
+
+      //From TestBeanGUI retrieve Bean Customizer as it includes all editors like ClassPropertyEditor, TableEditor
+      GenericTestBeanCustomizer testBeanCustomizer = (GenericTestBeanCustomizer) customizer.get(testBeanGUI);
+      Field editors = GenericTestBeanCustomizer.class.getDeclaredField(PropsKeysHelper.EDITORS);
+      editors.setAccessible(true);
+
+
+      //Retrieve TableEditor and set all fields with default values to it
+      PropertyEditor[] propertyEditors = (PropertyEditor[]) editors.get(testBeanCustomizer);
+      for (PropertyEditor propertyEditor : propertyEditors) {
+        if(propertyEditor instanceof JFileChooser){
+          String filePath = ((JFileChooser) propertyEditor).getSelectedFile().getPath();
+          try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
+            stream.forEach(System.out::println);
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+
+        }
+        if (propertyEditor instanceof TableEditor) {
+          propertyEditor.setValue(attributeList);
+        }
+      }
+      JOptionPane.showMessageDialog(null, "Successful retrieving of subject : " + subjectName, "Successful retrieving properties",
+          JOptionPane.INFORMATION_MESSAGE);
+    } catch (IOException | RestClientException | NoSuchFieldException | IllegalAccessException e) {
+      JOptionPane.showMessageDialog(null, "Failed retrieve schema properties : " + e.getMessage(), "ERROR: Failed to retrieve properties!",
+          JOptionPane.ERROR_MESSAGE);
+      log.error(e.getMessage(), e);
+    }
+  }
+
+  @Override
+  public void clearGui() {
+
+  }
+
+  @Override
+  public void setDescriptor(PropertyDescriptor descriptor) {
+    propertyDescriptor = descriptor;
+  }
+
+  @Override
+  public String getAsText() {
+    return Objects.requireNonNull(this.subjectNameComboBox.getSelectedItem()).toString();
+  }
+
+  @Override
+  public Component getCustomEditor() {
+    return this.panel;
+  }
+
+  @Override
+  public void setAsText(String text) throws IllegalArgumentException {
+    this.subjectNameComboBox.setSelectedItem(text);
+  }
+
+  @Override
+  public void setValue(Object value) {
+    if (value != null) {
+      this.subjectNameComboBox.setSelectedItem(value);
+    } else {
+      this.subjectNameComboBox.setSelectedItem("");
+    }
+
+  }
+
+  @Override
+  public Object getValue() {
+    return this.subjectNameComboBox.getSelectedItem();
+  }
+
+  @Override
+  public boolean supportsCustomEditor() {
+    return true;
+  }
+
+  class ComboFiller implements FocusListener {
+
+    @Override
+    public void focusGained(FocusEvent e) {
+      String subjects = JMeterContextService.getContext().getProperties().getProperty(SCHEMA_REGISTRY_SUBJECTS);
+      subjectNameComboBox.setModel(new DefaultComboBoxModel<>(subjects.split(",")));
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+      // Override but not used. Implementation not needed.
+    }
+  }
+
+}
