342fb0ba1be4257af6b4c651cda36d4ec0859ee1
Jose Enrique Garcia Macineiras <joseegman@ext.inditex.com>
2020-05-04 10:31:09 +0200

    Improve random map generation in RandomTool

--- src/test/java/net/coru/kloadgen/util/RandomToolTest.java
+++ src/test/java/net/coru/kloadgen/util/RandomToolTest.java
@@ -4,10 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.AbstractMap;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.UUID;
 import java.util.stream.Stream;
 import org.apache.avro.Schema.Field;
@@ -58,6 +60,45 @@ class RandomToolTest {
   void generateArrayRandomValue(String fieldType, Integer valueLength, List<String> fieldValuesList, Object expected) {
     assertThat((List<Object>)RandomTool.generateRandom(fieldType, valueLength, fieldValuesList))
         .allMatch(value -> value.equals(expected));
+  }
+
+  private static Stream<Arguments> parametersForGenerateMapRandomValueFromList() {
+    return Stream.of(
+        Arguments.of("string-map", 1, Collections.singletonList("testString:testString"), Maps.of("testString", "testString")),
+        Arguments.of("int-map", 1, Collections.singletonList("testString:1"),  Maps.of("testString",1)),
+        Arguments.of("long-map", 1, Collections.singletonList("testString:1"),  Maps.of("testString",1L)),
+        Arguments.of("short-map", 1, Collections.singletonList("testString:1"),  Maps.of("testString",(short)1)),
+        Arguments.of("double-map", 1, Collections.singletonList("testString:1.0"),  Maps.of("testString",1.0)),
+        Arguments.of("uuid-map", 1, Collections.singletonList("testString:0177f035-e51c-4a46-8b82-5b157371c2a5"),  Maps.of("testString", UUID.fromString("0177f035-e51c-4a46-8b82-5b157371c2a5")))
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("parametersForGenerateMapRandomValueFromList")
+  void generateMapRandomValueFromList(String fieldType, Integer valueLength, List<String> fieldValuesList, Map<String, Object> expected) {
+    Map.Entry<String, Object>[] expectedMap = expected.entrySet().toArray(new Map.Entry[1]);
+    assertThat((Map<String, Object>)RandomTool.generateRandom(fieldType, valueLength, fieldValuesList))
+        .containsExactly(expectedMap);
+  }
+
+  private static Stream<Arguments> parametersForGenerateMapFixedKeyRandomValue() {
+    return Stream.of(
+        Arguments.of("string-map", 1, Collections.singletonList("testString")),
+        Arguments.of("int-map", 1, Collections.singletonList("testString")),
+        Arguments.of("long-map", 1, Collections.singletonList("testString")),
+        Arguments.of("short-map", 1, Collections.singletonList("testString")),
+        Arguments.of("double-map", 1, Collections.singletonList("testString")),
+        Arguments.of("uuid-map", 1, Collections.singletonList("testString"))
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("parametersForGenerateMapFixedKeyRandomValue")
+  void generateMapFixedKeyRandomValue(String fieldType, Integer valueLength, List<String> fieldValuesList) {
+    String[] expectedKeys = fieldValuesList.toArray(new String[1]);
+    Map<String, Object> result = (Map<String, Object>)RandomTool.generateRandom(fieldType, valueLength, fieldValuesList);
+    assertThat(result).containsKeys(expectedKeys);
+    assertThat(result).doesNotContainValue(null);
   }
 
   private static Stream<Arguments> parametersForGenerateRandomValueForField() {
