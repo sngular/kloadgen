9e220ab13cdd1e9c60d7dc3f128bc2cf113b970a
fjaenni <64318698+fjaenni@users.noreply.github.com>
2020-05-13 22:44:03 +0200

    new fields types boolean, fixed and bytes (#21)
    * new fields types boolean, fixed and bytes
    
    * pom bug
    
    * refactor methods
    
    Co-authored-by: franciscojn <franciscojn@ext.inditex.com>

--- src/main/java/net/coru/kloadgen/util/AvroRandomTool.java
+++ src/main/java/net/coru/kloadgen/util/AvroRandomTool.java
@@ -1,6 +1,7 @@
 package net.coru.kloadgen.util;
 
 import static org.apache.avro.Schema.Type.ENUM;
+import static org.apache.avro.Schema.Type.FIXED;
 import static org.apache.avro.Schema.Type.NULL;
 import static org.apache.avro.Schema.Type.UNION;
 
@@ -8,10 +9,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.apache.avro.Schema;
 import org.apache.avro.Schema.Field;
 import org.apache.avro.Schema.Type;
 import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericFixed;
 import org.apache.commons.collections4.IterableUtils;
 import org.apache.commons.lang3.RandomUtils;
 import org.apache.jmeter.threads.JMeterContextService;
@@ -47,6 +50,9 @@ public class AvroRandomTool {
     } else if (differentTypesNeedCast(fieldType, field.schema().getType())) {
       value = RandomTool.castValue(value, field.schema().getType().getName());
     }
+	if(FIXED == field.schema().getType()) {
+		value = getFixedOrGenerate(field.schema());
+	}
     return value;
   }
 
@@ -74,6 +80,13 @@ public class AvroRandomTool {
         return !fieldTypeSchema.getName().equals(fieldType);
     }
   }
+  
+  private static GenericFixed getFixedOrGenerate( Schema schema) {
+	
+		byte[] bytes = new byte[schema.getFixedSize()];
+
+		return new GenericData.Fixed(schema, bytes);
+	}
 
   private static boolean needCastForInt(String fieldType) {
 
@@ -111,4 +124,6 @@ public class AvroRandomTool {
   private Schema getRecordUnion(List<Schema> types) {
     return IterableUtils.find(types, schema -> !schema.getType().equals(NULL));
   }
+  
+	
 }
