344259072c09455c9c6816f8fc164e386805d4ab
kszosze <kszosze@gmail.com>
2021-02-25 23:54:33 +0100

    Add Simple Plain key configuration. Small refactor.

--- src/main/java/net/coru/kloadgen/sampler/KafkaSampler.java
+++ src/main/java/net/coru/kloadgen/sampler/KafkaSampler.java
@@ -9,16 +9,16 @@ package net.coru.kloadgen.sampler;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static net.coru.kloadgen.util.ProducerKeysHelper.ENABLE_AUTO_SCHEMA_REGISTRATION_CONFIG;
-import static net.coru.kloadgen.util.ProducerKeysHelper.FLAG_YES;
 import static net.coru.kloadgen.util.ProducerKeysHelper.KAFKA_HEADERS;
 import static net.coru.kloadgen.util.ProducerKeysHelper.KAFKA_TOPIC_CONFIG;
 import static net.coru.kloadgen.util.ProducerKeysHelper.KEY_SERIALIZER_CLASS_CONFIG_DEFAULT;
 import static net.coru.kloadgen.util.ProducerKeysHelper.VALUE_NAME_STRATEGY;
-import static net.coru.kloadgen.util.PropsKeysHelper.KEYED_MESSAGE_KEY;
 import static net.coru.kloadgen.util.PropsKeysHelper.KEY_SUBJECT_NAME;
 import static net.coru.kloadgen.util.PropsKeysHelper.MESSAGE_KEY_KEY_TYPE;
 import static net.coru.kloadgen.util.PropsKeysHelper.MESSAGE_KEY_KEY_VALUE;
 import static net.coru.kloadgen.util.PropsKeysHelper.MSG_KEY_VALUE;
+import static net.coru.kloadgen.util.PropsKeysHelper.SCHEMA_KEYED_MESSAGE_KEY;
+import static net.coru.kloadgen.util.PropsKeysHelper.SIMPLE_KEYED_MESSAGE_KEY;
 import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
@@ -74,14 +74,15 @@ public class KafkaSampler extends AbstractJavaSamplerClient implements Serializa
         props = properties(context);
         generator = SamplerUtil.configureValueGenerator(props);
 
-        if (FLAG_YES.equals(context.getParameter(KEYED_MESSAGE_KEY))) {
+        if ("true".equals(context.getJMeterVariables().get(SCHEMA_KEYED_MESSAGE_KEY)) ||
+            "true".equals(context.getJMeterVariables().get(SIMPLE_KEYED_MESSAGE_KEY))) {
             keyMessageFlag = true;
             if (!Objects.isNull(JMeterContextService.getContext().getVariables().get(KEY_SUBJECT_NAME))) {
                 keyGenerator = SamplerUtil.configureKeyGenerator(props);
             } else {
-                msgKeyType = context.getParameter(MESSAGE_KEY_KEY_TYPE);
-                msgKeyValue = MSG_KEY_VALUE.equalsIgnoreCase(context.getParameter(MESSAGE_KEY_KEY_VALUE))
-                    ? emptyList() : singletonList(context.getParameter(MESSAGE_KEY_KEY_VALUE));
+                msgKeyType = props.getProperty(MESSAGE_KEY_KEY_TYPE);
+                msgKeyValue = MSG_KEY_VALUE.equalsIgnoreCase(props.getProperty(MESSAGE_KEY_KEY_VALUE))
+                    ? emptyList() : singletonList(props.getProperty(MESSAGE_KEY_KEY_VALUE));
             }
         } else {
             props.put(KEY_SERIALIZER_CLASS_CONFIG, KEY_SERIALIZER_CLASS_CONFIG_DEFAULT);
@@ -123,7 +124,7 @@ public class KafkaSampler extends AbstractJavaSamplerClient implements Serializa
 
             ProducerRecord<Object, Object> producerRecord;
             try {
-                producerRecord = getProducerRecord(messageVal, enrichedKeyFlag(jMeterContext), enrichedValueFlag(jMeterContext));
+                producerRecord = getProducerRecord(messageVal, enrichedKeyFlag(), enrichedValueFlag());
                 List<String> headersSB = new ArrayList<>(SamplerUtil.populateHeaders(kafkaHeaders, producerRecord));
 
                 sampleResult.setRequestHeaders(StringUtils.join(headersSB, ","));
@@ -136,7 +137,8 @@ public class KafkaSampler extends AbstractJavaSamplerClient implements Serializa
                     }
                 });
 
-                super.getNewLogger().info("Send message to body: {}", producerRecord.value());
+                super.getNewLogger().info("Send message with key: {} and body: {} and headers: {}",
+                    producerRecord.key(), producerRecord.value(), producerRecord.headers());
                 fillSampleResult(sampleResult, prettyPrint(result.get()), true);
             } catch (Exception e) {
                 super.getNewLogger().error("Failed to send message", e);
@@ -149,12 +151,12 @@ public class KafkaSampler extends AbstractJavaSamplerClient implements Serializa
         return sampleResult;
     }
 
-    private Boolean enrichedValueFlag(JMeterContext jMeterContext) {
-        return props.get(VALUE_SERIALIZER_CLASS_CONFIG).equals(AvroSerializer.class.getName());
+    private Boolean enrichedValueFlag() {
+        return AvroSerializer.class.getName().equals(props.get(VALUE_SERIALIZER_CLASS_CONFIG));
     }
 
-    private Boolean enrichedKeyFlag(JMeterContext jMeterContext) {
-        return props.get(KEY_SERIALIZER_CLASS_CONFIG).equals(AvroSerializer.class.getName());
+    private Boolean enrichedKeyFlag() {
+        return AvroSerializer.class.getName().equals(props.get(KEY_SERIALIZER_CLASS_CONFIG));
     }
 
     private void fillSamplerResult(ProducerRecord<Object, Object> producerRecord, SampleResult sampleResult) {
