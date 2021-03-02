a75bb9eb9091a705bc94799e688b5965adbf9dbc
kszosze <kszosze@gmail.com>
2021-03-02 20:54:14 +0100

    #85 Adding serializer to Schema Config elements

--- src/main/java/net/coru/kloadgen/config/keyserialized/KeySerializedConfigElementBeanInfo.java
+++ src/main/java/net/coru/kloadgen/config/keyserialized/KeySerializedConfigElementBeanInfo.java
@@ -9,6 +9,7 @@ package net.coru.kloadgen.config.keyserialized;
 import java.beans.PropertyDescriptor;
 import java.util.ArrayList;
 import net.coru.kloadgen.model.FieldValueMapping;
+import net.coru.kloadgen.property.editor.KeySerializerPropertyEditor;
 import net.coru.kloadgen.property.editor.SchemaTypePropertyEditor;
 import net.coru.kloadgen.property.editor.SerialisedSubjectPropertyEditor;
 import org.apache.jmeter.testbeans.BeanInfoSupport;
@@ -17,49 +18,57 @@ import org.apache.jmeter.testbeans.gui.TypeEditor;
 
 public class KeySerializedConfigElementBeanInfo extends BeanInfoSupport {
 
-    private static final String KEY_SUBJECT_NAME = "keySubjectName";
+  private static final String KEY_SUBJECT_NAME = "keySubjectName";
 
-    private static final String KEY_SCHEMA_PROPERTIES = "keySchemaProperties";
+  private static final String KEY_SCHEMA_PROPERTIES = "keySchemaProperties";
 
-    private static final String KEY_SCHEMA_TYPE = "keySchemaType";
+  private static final String KEY_SCHEMA_TYPE = "keySchemaType";
 
-    public KeySerializedConfigElementBeanInfo() {
+  private static final String KEY_SERIALIZER_PROPERTY = "keySerializerConfiguration";
 
-        super(KeySerializedConfigElement.class);
+  public KeySerializedConfigElementBeanInfo() {
 
-        createPropertyGroup("key_serialized_load_generator", new String[] {
-            KEY_SUBJECT_NAME, KEY_SCHEMA_PROPERTIES, KEY_SCHEMA_TYPE
-        });
+    super(KeySerializedConfigElement.class);
 
-        TypeEditor tableEditor = TypeEditor.TableEditor;
-        PropertyDescriptor tableProperties = property(KEY_SCHEMA_PROPERTIES, tableEditor);
-        tableProperties.setValue(TableEditor.CLASSNAME, FieldValueMapping.class.getName());
-        tableProperties.setValue(TableEditor.HEADERS,
-            new String[]{
-                "Field Name",
-                "Field Type",
-                "Field Length",
-                "Field Values List"
-            });
-        tableProperties.setValue(TableEditor.OBJECT_PROPERTIES,
-            new String[]{
-                FieldValueMapping.FIELD_NAME,
-                FieldValueMapping.FIELD_TYPE,
-                FieldValueMapping.VALUE_LENGTH,
-                FieldValueMapping.FIELD_VALUES_LIST
-            });
-        tableProperties.setValue(DEFAULT, new ArrayList<>());
-        tableProperties.setValue(NOT_UNDEFINED, Boolean.TRUE);
+    createPropertyGroup("key_serialized_load_generator", new String[] {
+        KEY_SERIALIZER_PROPERTY, KEY_SUBJECT_NAME, KEY_SCHEMA_PROPERTIES, KEY_SCHEMA_TYPE
+    });
 
-        PropertyDescriptor subjectNameProps = property(KEY_SUBJECT_NAME);
-        subjectNameProps.setPropertyEditorClass(SerialisedSubjectPropertyEditor.class);
-        subjectNameProps.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        subjectNameProps.setValue(NOT_EXPRESSION, Boolean.FALSE);
+    PropertyDescriptor serializerPropertyProps = property(KEY_SERIALIZER_PROPERTY);
+    serializerPropertyProps.setPropertyEditorClass(KeySerializerPropertyEditor.class);
+    serializerPropertyProps.setValue(NOT_UNDEFINED, Boolean.TRUE);
+    serializerPropertyProps.setValue(DEFAULT, "");
+    serializerPropertyProps.setValue(NOT_EXPRESSION, Boolean.FALSE);
 
-        PropertyDescriptor schemaType = property(KEY_SCHEMA_TYPE);
-        schemaType.setPropertyEditorClass(SchemaTypePropertyEditor.class);
-        schemaType.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        schemaType.setValue(DEFAULT, "<avro subject>");
-        schemaType.setValue(NOT_EXPRESSION, Boolean.FALSE);
-    }
+    TypeEditor tableEditor = TypeEditor.TableEditor;
+    PropertyDescriptor tableProperties = property(KEY_SCHEMA_PROPERTIES, tableEditor);
+    tableProperties.setValue(TableEditor.CLASSNAME, FieldValueMapping.class.getName());
+    tableProperties.setValue(TableEditor.HEADERS,
+       new String[]{
+           "Field Name",
+           "Field Type",
+           "Field Length",
+           "Field Values List"
+       });
+    tableProperties.setValue(TableEditor.OBJECT_PROPERTIES,
+       new String[]{
+           FieldValueMapping.FIELD_NAME,
+           FieldValueMapping.FIELD_TYPE,
+           FieldValueMapping.VALUE_LENGTH,
+           FieldValueMapping.FIELD_VALUES_LIST
+       });
+    tableProperties.setValue(DEFAULT, new ArrayList<>());
+    tableProperties.setValue(NOT_UNDEFINED, Boolean.TRUE);
+
+    PropertyDescriptor subjectNameProps = property(KEY_SUBJECT_NAME);
+    subjectNameProps.setPropertyEditorClass(SerialisedSubjectPropertyEditor.class);
+    subjectNameProps.setValue(NOT_UNDEFINED, Boolean.TRUE);
+    subjectNameProps.setValue(NOT_EXPRESSION, Boolean.FALSE);
+
+    PropertyDescriptor schemaType = property(KEY_SCHEMA_TYPE);
+    schemaType.setPropertyEditorClass(SchemaTypePropertyEditor.class);
+    schemaType.setValue(NOT_UNDEFINED, Boolean.TRUE);
+    schemaType.setValue(DEFAULT, "<avro subject>");
+    schemaType.setValue(NOT_EXPRESSION, Boolean.FALSE);
+  }
 }
