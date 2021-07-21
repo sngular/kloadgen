6197bc71f6ee31e4b9bf17c1cc90a3cbd79a2103
tiagofds <tiagofds@ext.zara.com>
2021-07-21 16:12:10 +0200

    #105 - Update classes RandomArray and RandomMap

--- src/main/java/net/coru/kloadgen/randomtool/random/RandomArray.java
+++ src/main/java/net/coru/kloadgen/randomtool/random/RandomArray.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import net.coru.kloadgen.model.ConstraintTypeEnum;
 import net.coru.kloadgen.randomtool.util.ValidTypes;
 import org.apache.commons.lang3.RandomUtils;
@@ -14,135 +13,55 @@ public class RandomArray {
   private final RandomObject randomObject;
 
   public RandomArray() {
-    randomObject  = new RandomObject();
+    randomObject = new RandomObject();
   }
 
   public Object generateArray(String fieldType, Integer valueLength, List<String> fieldValueList, Integer arraySize,
       Map<ConstraintTypeEnum, String> constrains) {
     Object value;
+
     switch (fieldType) {
       case ValidTypes.INT_ARRAY:
-        value = generateIntArray(arraySize, valueLength, fieldValueList, constrains);
+        value = generate(ValidTypes.INT, arraySize, valueLength, fieldValueList, constrains);
         break;
       case ValidTypes.LONG_ARRAY:
-        value = generateLongArray(arraySize, valueLength, fieldValueList, constrains);
+        value = generate(ValidTypes.LONG, arraySize, valueLength, fieldValueList, constrains);
         break;
       case ValidTypes.DOUBLE_ARRAY:
-        value = generateDoubleArray(arraySize, valueLength, fieldValueList, constrains);
+        value = generate(ValidTypes.DOUBLE, arraySize, valueLength, fieldValueList, constrains);
         break;
       case ValidTypes.SHORT_ARRAY:
-        value = generateShortArray(arraySize, valueLength, fieldValueList, constrains);
+        value = generate(ValidTypes.SHORT, arraySize, valueLength, fieldValueList, constrains);
         break;
       case ValidTypes.FLOAT_ARRAY:
-        value = generateFloatArray(arraySize, valueLength, fieldValueList, constrains);
+        value = generate(ValidTypes.FLOAT, arraySize, valueLength, fieldValueList, constrains);
         break;
       case ValidTypes.STRING_ARRAY:
-        value = generateStringArray(arraySize, valueLength, fieldValueList, constrains);
+        value = generate(ValidTypes.STRING, arraySize, valueLength, fieldValueList, constrains);
         break;
       case ValidTypes.UUID_ARRAY:
-        value = generateUuidArray(arraySize, fieldValueList);
+        value = generate(ValidTypes.UUID, arraySize, 0, fieldValueList, Collections.emptyMap());
         break;
       case ValidTypes.BOOLEAN_ARRAY:
-        value = generateBooleanArray(arraySize, fieldValueList);
+        value = generate(ValidTypes.BOOLEAN, arraySize, 0, fieldValueList, Collections.emptyMap());
         break;
       default:
         value = new ArrayList<>();
         break;
     }
+
     return value;
   }
 
-  private List<Integer> generateIntArray(Integer arraySize, Integer valueLength, List<String> fieldValueList,
-      Map<ConstraintTypeEnum, String> constrains) {
-    int size = arraySize == 0 ? RandomUtils.nextInt(1,5) : arraySize;
-    List<Integer> intArray = new ArrayList<>();
-    for (int i=0; i<size; i++) {
-      intArray.add(
-          (Integer) randomObject.generateRandom(ValidTypes.INT, valueLength, fieldValueList, constrains)
-      );
-    }
-    return intArray;
-  }
-
-  private List<Long> generateLongArray(Integer arraySize, Integer valueLength, List<String> fieldValueList,
-      Map<ConstraintTypeEnum, String> constrains) {
-    int size = arraySize == 0 ? RandomUtils.nextInt(1,5) : arraySize;
-    List<Long> longArray = new ArrayList<>();
-    for (int i=0; i<size; i++) {
-      longArray.add(
-          (Long) randomObject.generateRandom(ValidTypes.LONG, valueLength, fieldValueList, constrains)
-      );
-    }
-    return longArray;
-  }
-
-  private List<Double> generateDoubleArray(Integer arraySize, Integer valueLength, List<String> fieldValueList,
-      Map<ConstraintTypeEnum, String> constrains) {
-    int size = arraySize == 0 ? RandomUtils.nextInt(1,5) : arraySize;
-    List<Double> doubleArray = new ArrayList<>();
-    for (int i=0; i<size; i++) {
-      doubleArray.add(
-          (Double) randomObject.generateRandom(ValidTypes.DOUBLE, valueLength, fieldValueList, constrains)
-      );
-    }
-    return doubleArray;
-  }
-
-  private List<Short> generateShortArray(Integer arraySize, Integer valueLength, List<String> fieldValueList,
+  private List<Object> generate(String type, Integer arraySize, Integer valueLength, List<String> fieldValueList,
       Map<ConstraintTypeEnum, String> constrains) {
     int size = arraySize == 0 ? RandomUtils.nextInt(1, 5) : arraySize;
-    List<Short> shortArray = new ArrayList<>();
+    List<Object> array = new ArrayList<>(size);
+
     for (int i = 0; i < size; i++) {
-      shortArray.add(
-          (Short) randomObject.generateRandom(ValidTypes.SHORT, valueLength, fieldValueList, constrains)
-      );
+      array.add(randomObject.generateRandom(type, valueLength, fieldValueList, constrains));
     }
-    return shortArray;
-  }
 
-  private List<Float> generateFloatArray(Integer arraySize, Integer valueLength, List<String> fieldValueList,
-      Map<ConstraintTypeEnum, String> constrains) {
-    int size = arraySize == 0 ? RandomUtils.nextInt(1, 5) : arraySize;
-    List<Float> floatArray = new ArrayList<>();
-    for (int i = 0; i < size; i++) {
-      floatArray.add(
-          (Float) randomObject.generateRandom(ValidTypes.FLOAT, valueLength, fieldValueList, constrains)
-      );
-    }
-    return floatArray;
-  }
-
-  private List<String> generateStringArray(Integer arraySize, Integer valueLength, List<String> fieldValueList,
-      Map<ConstraintTypeEnum, String> constrains) {
-    int size = arraySize == 0 ? RandomUtils.nextInt(1, 5) : arraySize;
-    List<String> stringArray = new ArrayList<>();
-    for (int i = 0; i < size; i++) {
-      stringArray.add(
-          (String) randomObject.generateRandom(ValidTypes.STRING, valueLength, fieldValueList, constrains)
-      );
-    }
-    return stringArray;
-  }
-
-  private List<UUID> generateUuidArray(Integer arraySize, List<String> fieldValueList) {
-    int size = arraySize == 0 ? RandomUtils.nextInt(1,5) : arraySize;
-    List<UUID> uuidArray = new ArrayList<>();
-    for (int i=0; i<size; i++) {
-      uuidArray.add(
-          (UUID) randomObject.generateRandom(ValidTypes.UUID, 0, fieldValueList, Collections.emptyMap())
-      );
-    }
-    return uuidArray;
-  }
-
-  private List<Boolean> generateBooleanArray(Integer arraySize, List<String> fieldValueList) {
-    int size = arraySize == 0 ? RandomUtils.nextInt(1,5) : arraySize;
-    List<Boolean> booleanArray = new ArrayList<>();
-    for (int i=0; i<size; i++) {
-      booleanArray.add(
-          (Boolean) randomObject.generateRandom(ValidTypes.BOOLEAN, 0, fieldValueList, Collections.emptyMap())
-      );
-    }
-    return booleanArray;
+    return array;
   }
 }
