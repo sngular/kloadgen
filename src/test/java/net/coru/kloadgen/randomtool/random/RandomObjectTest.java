fb34953524832e56c045bf7082faf188536cb449
pboullosa <pboullosa@corunet.com>
2021-08-10 11:40:28 +0200

    #110 Random logical type test

--- src/test/java/net/coru/kloadgen/randomtool/random/RandomObjectTest.java
+++ src/test/java/net/coru/kloadgen/randomtool/random/RandomObjectTest.java
@@ -2,13 +2,16 @@ package net.coru.kloadgen.randomtool.random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneOffset;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 import org.apache.groovy.util.Maps;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,7 +20,22 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class RandomObjectTest {
 
-  private static final LocalDateTime FIXED_DATE = LocalDateTime.of(2019, 12, 6, 12, 0, 0);
+  private static final LocalDateTime FIXED_TIMESTAMP = LocalDateTime.of(2019, 12, 6, 10, 15, 30);
+  private static final LocalDate FIXED_DATE = LocalDate.of(2019,12,6);
+  private static final LocalTime FIXED_TIME = LocalTime.of(10,15,30);
+  private static final String TIMESTAMP_STRING = "2019-12-06T10:15:30";
+  private static final String DATE_STRING = "2019-12-06";
+  private static final String TIME_STRING = "10:15:30";
+
+  private static long getMillisFromDate (LocalDateTime localDateTime){
+    return TimeUnit.MILLISECONDS.convert(localDateTime.toEpochSecond(ZoneOffset.UTC), TimeUnit.SECONDS) +
+            TimeUnit.MILLISECONDS.convert(localDateTime.getNano(), TimeUnit.NANOSECONDS);
+  }
+
+  private static long getMicrosFromDate (LocalDateTime localDateTime){
+    return TimeUnit.MICROSECONDS.convert(localDateTime.toEpochSecond(ZoneOffset.UTC), TimeUnit.SECONDS) +
+            TimeUnit.MICROSECONDS.convert(localDateTime.getNano(), TimeUnit.NANOSECONDS);
+  }
 
   private static Stream<Arguments> parametersForGenerateSingleRandomValue() {
     return Stream.of(
@@ -27,16 +45,34 @@ class RandomObjectTest {
         Arguments.of("short", 1, Collections.singletonList("1"), (short) 1),
         Arguments.of("double", 1, Collections.singletonList("1.0"), 1.0),
         Arguments.of("float", 1, Collections.singletonList("1.0"), 1.0f),
-        Arguments.of("timestamp", 1, Collections.singletonList("2019-12-06T12:00:00"), FIXED_DATE),
+        Arguments.of("timestamp", 1, Collections.singletonList(TIMESTAMP_STRING), FIXED_TIMESTAMP),
         Arguments.of(
-            "longTimestamp", 1, Collections.singletonList("2019-12-06T12:00:00"), FIXED_DATE.toInstant(ZoneOffset.UTC).toEpochMilli()
+            "longTimestamp", 1, Collections.singletonList(TIMESTAMP_STRING), FIXED_TIMESTAMP.toInstant(ZoneOffset.UTC).toEpochMilli()
         ),
-        Arguments.of("stringTimestamp", 1, Collections.singletonList("2019-12-06T12:00:00"), "2019-12-06T12:00"),
+        Arguments.of("stringTimestamp", 1, Collections.singletonList(TIMESTAMP_STRING), TIMESTAMP_STRING),
         Arguments.of(
             "uuid", 1, Collections.singletonList("0177f035-e51c-4a46-8b82-5b157371c2a5"),
             UUID.fromString("0177f035-e51c-4a46-8b82-5b157371c2a5")
         ),
         Arguments.of("boolean", 1, Collections.singletonList("true"), Boolean.TRUE)
+    );
+  }
+
+  private static Stream<Arguments> parametersForGenerateSingleLogicalTypeRandomValue(){
+    return Stream.of(
+            Arguments.of("int_date", 1, Collections.singletonList(DATE_STRING), (int) FIXED_DATE.toEpochDay()),
+            Arguments.of("int_time-millis", 1, Collections.singletonList(TIME_STRING),
+                    (int) TimeUnit.MILLISECONDS.convert(FIXED_TIME.toNanoOfDay(), TimeUnit.NANOSECONDS)),
+            Arguments.of("long_time-micros", 1, Collections.singletonList(TIME_STRING),
+                    TimeUnit.MICROSECONDS.convert(FIXED_TIME.toNanoOfDay(), TimeUnit.NANOSECONDS)),
+            Arguments.of("long_timestamp-millis", 1, Collections.singletonList(TIMESTAMP_STRING),
+                    getMillisFromDate(FIXED_TIMESTAMP)),
+            Arguments.of("long_timestamp-micros", 1, Collections.singletonList(TIMESTAMP_STRING),
+                    getMicrosFromDate(FIXED_TIMESTAMP)),
+            Arguments.of("long_local-timestamp-millis", 1, Collections.singletonList(TIMESTAMP_STRING),
+                    getMillisFromDate(FIXED_TIMESTAMP)),
+            Arguments.of("long_local-timestamp-micros", 1, Collections.singletonList(TIMESTAMP_STRING),
+                    getMicrosFromDate(FIXED_TIMESTAMP))
     );
   }
 
@@ -49,6 +85,12 @@ class RandomObjectTest {
   @ParameterizedTest
   @MethodSource("parametersForGenerateSingleRandomValue")
   void generateSingleRandomValue(String fieldType, Integer valueLength, List<String> fieldValuesList, Object expected) {
+    assertThat(new RandomObject().generateRandom(fieldType, valueLength, fieldValuesList, Collections.emptyMap())).isEqualTo(expected);
+  }
+
+  @ParameterizedTest
+  @MethodSource("parametersForGenerateSingleLogicalTypeRandomValue")
+  void generateSingleLogicalTypeRandomValue(String fieldType, Integer valueLength, List<String> fieldValuesList, Object expected) {
     assertThat(new RandomObject().generateRandom(fieldType, valueLength, fieldValuesList, Collections.emptyMap())).isEqualTo(expected);
   }
 
