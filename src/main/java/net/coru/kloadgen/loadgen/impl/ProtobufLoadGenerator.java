124321ae220bcff8bfb720778f369cee3ec44dfb
kszosze <kszosze@gmail.com>
2022-01-28 13:28:04 +0100

    fix generator.

--- src/main/java/net/coru/kloadgen/loadgen/impl/ProtobufLoadGenerator.java
+++ src/main/java/net/coru/kloadgen/loadgen/impl/ProtobufLoadGenerator.java
@@ -12,13 +12,13 @@ import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.SchemaMetadata;
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
+import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchema;
 import lombok.extern.slf4j.Slf4j;
 import net.coru.kloadgen.exception.KLoadGenException;
 import net.coru.kloadgen.loadgen.BaseLoadGenerator;
 import net.coru.kloadgen.model.FieldValueMapping;
 import net.coru.kloadgen.processor.ProtobufSchemaProcessor;
 import net.coru.kloadgen.serializer.EnrichedRecord;
-import org.apache.avro.Schema;
 
 @Slf4j
 public class ProtobufLoadGenerator implements BaseLoadGenerator {
@@ -46,8 +46,8 @@ public class ProtobufLoadGenerator implements BaseLoadGenerator {
   @Override
   public void setUpGenerator(String schema , List<FieldValueMapping> fieldExprMappings) {
     try {
-      Schema.Parser parser = new Schema.Parser();
-      this.protobufSchemaProcessor.processSchema((ParsedSchema) parser.parse(schema) , new SchemaMetadata(1 , 1 , schema) , fieldExprMappings);
+      ProtobufSchema protobufSchema = new ProtobufSchema(schema);
+      this.protobufSchemaProcessor.processSchema(protobufSchema, new SchemaMetadata(1, 1, schema), fieldExprMappings);
     } catch (Exception exc) {
       log.error("Please make sure that properties data type and expression function return type are compatible with each other" , exc);
       throw new KLoadGenException(exc);
