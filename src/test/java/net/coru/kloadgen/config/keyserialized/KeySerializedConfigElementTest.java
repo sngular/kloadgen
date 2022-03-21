aa07aab43ad9b7ae44fe78d58d702f05efb28b25
Jose E. Garcia Maciñeiras <68995937+jemacineiras@users.noreply.github.com>
2022-03-21 10:22:04 +0100

    #134 move release to maven central (#194)
    * Tries to add settings profile to publish on MVN central
    
    * #134 Reorganize POM
    
    * Tries to add settings profile to publish on MVN central
    
    * #134 Reorganize POM
    
    * #75 Fix Extractor and Processor due Enum
    
    * #75 Fix One Of values generation
    
    * #134 Adding secrets
    
    * #134 Adding secrets
    
    * #134 Maven version bump
    
    Co-authored-by: Adrián Mosquera <amosquera@corunet.com>
    Co-authored-by: kszosze <kszosze@gmail.com>

--- src/test/java/net/coru/kloadgen/config/keyserialized/KeySerializedConfigElementTest.java
+++ src/test/java/net/coru/kloadgen/config/keyserialized/KeySerializedConfigElementTest.java
@@ -13,27 +13,23 @@ import static net.coru.kloadgen.util.SchemaRegistryKeyHelper.SCHEMA_REGISTRY_URL
 import static net.coru.kloadgen.util.SchemaRegistryKeyHelper.SCHEMA_REGISTRY_USERNAME_KEY;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import io.confluent.kafka.serializers.subject.TopicNameStrategy;
 import java.io.File;
 import java.util.Collections;
 import java.util.Locale;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import io.confluent.kafka.serializers.subject.TopicNameStrategy;
 import net.coru.kloadgen.serializer.AvroSerializer;
 import org.apache.jmeter.threads.JMeterContext;
 import org.apache.jmeter.threads.JMeterContextService;
 import org.apache.jmeter.threads.JMeterVariables;
 import org.apache.jmeter.util.JMeterUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import ru.lanwen.wiremock.ext.WiremockResolver;
-import ru.lanwen.wiremock.ext.WiremockResolver.Wiremock;
-import ru.lanwen.wiremock.ext.WiremockUriResolver;
 
-@ExtendWith({
-    WiremockResolver.class,
-    WiremockUriResolver.class
-})
+@WireMockTest
 class KeySerializedConfigElementTest {
 
   @BeforeEach
@@ -47,15 +43,16 @@ class KeySerializedConfigElementTest {
   }
 
   @Test
-  public void iterationStart( @Wiremock WireMockServer server) {
+  @DisplayName("Should configure Key Serialized Properties")
+  void iterationStart(WireMockRuntimeInfo wmRuntimeInfo) {
 
-    JMeterContextService.getContext().getProperties().put(SCHEMA_REGISTRY_URL, "http://localhost:" + server.port());
+    JMeterContextService.getContext().getProperties().put(SCHEMA_REGISTRY_URL, wmRuntimeInfo.getHttpBaseUrl());
     JMeterContextService.getContext().getProperties().put(SCHEMA_REGISTRY_USERNAME_KEY, "foo");
     JMeterContextService.getContext().getProperties().put(SCHEMA_REGISTRY_PASSWORD_KEY, "foo");
 
     KeySerializedConfigElement
         keySerializedConfigElement = new KeySerializedConfigElement("avroSubject", Collections.emptyList(), "AVRO",
-           AvroSerializer.class.getSimpleName(), TopicNameStrategy.class.getSimpleName());
+                                                                    AvroSerializer.class.getSimpleName(), TopicNameStrategy.class.getSimpleName());
     keySerializedConfigElement.iterationStart(null);
     assertThat(JMeterContextService.getContext().getVariables().getObject(KEY_SUBJECT_NAME)).isNotNull();
     assertThat(JMeterContextService.getContext().getVariables().getObject(KEY_SCHEMA_PROPERTIES)).isNotNull();
