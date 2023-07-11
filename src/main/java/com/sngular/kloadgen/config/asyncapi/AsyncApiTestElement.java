44eea5f423a39e7dc14043ff276aa2f708b1dcb5
Jose Enrique García Maciñeiras <joseenrique.garcia@sngular.com>
2023-07-11 08:39:01 +0200

    Fixing the code

--- src/main/java/com/sngular/kloadgen/config/asyncapi/AsyncApiTestElement.java
+++ src/main/java/com/sngular/kloadgen/config/asyncapi/AsyncApiTestElement.java
@@ -4,14 +4,14 @@ import org.apache.jmeter.config.Arguments;
 
 public class AsyncApiTestElement extends Arguments {
 
-    public static final String FILENAME = "filename";
-    public static final String BROKER = "broker";
-    public static final String SCHEMA_REGISTRY = "schema_registry";
-    public static final String CHANNEL = "channel";
-    public static final String MESSAGE = "message";
-    public static final String KAFKA_KEY = "kafka.key";
-    public static final String KAFKA_PAYLOAD = "kafka_payload";
+  public static final String FILENAME = "filename";
+  public static final String BROKER = "broker";
+  public static final String SCHEMA_REGISTRY = "schema_registry";
+  public static final String CHANNEL = "channel";
+  public static final String MESSAGE = "message";
+  public static final String KAFKA_KEY = "kafka.key";
+  public static final String KAFKA_PAYLOAD = "kafka_payload";
 
-    
+
 
 }
