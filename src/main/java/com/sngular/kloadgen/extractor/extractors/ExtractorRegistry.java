a3cad52b71826a020ca7546e24731baf4d2e1897
Jose Enrique García Maciñeiras <joseenrique.garcia@sngular.com>
2023-10-03 10:54:19 +0200

    #332 Fix styles and pom version

--- src/main/java/com/sngular/kloadgen/extractor/extractors/ExtractorRegistry.java
+++ src/main/java/com/sngular/kloadgen/extractor/extractors/ExtractorRegistry.java
@@ -1,6 +1,7 @@
 package com.sngular.kloadgen.extractor.extractors;
 
 import java.util.List;
+
 import com.sngular.kloadgen.common.SchemaRegistryEnum;
 import com.sngular.kloadgen.model.FieldValueMapping;
 
