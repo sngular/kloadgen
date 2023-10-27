ed43cefd3d0a407545b474d668d7d06e0934b9d4
Graciela Méndez Olmos <graciela.mendez@sngular.com>
2023-10-27 12:51:55 +0200

    Fix AvroSchema cast

--- src/main/java/com/sngular/kloadgen/extractor/extractors/avro/AvroExtractor.java
+++ src/main/java/com/sngular/kloadgen/extractor/extractors/avro/AvroExtractor.java
@@ -10,14 +10,22 @@ import com.sngular.kloadgen.common.SchemaRegistryEnum;
 import com.sngular.kloadgen.extractor.extractors.Extractor;
 import com.sngular.kloadgen.extractor.extractors.ExtractorRegistry;
 import com.sngular.kloadgen.model.FieldValueMapping;
-
+//import com.sngular.kloadgen.model.json.Schema;
+import io.confluent.kafka.schemaregistry.avro.AvroSchema;
+import org.apache.avro.Schema;
 public class AvroExtractor implements ExtractorRegistry<Object> {
 
   static Map<SchemaRegistryEnum, Extractor> schemaRegistryMap = Map.of(CONFLUENT, new AvroConfluentExtractor(), APICURIO,
           new AvroApicurioExtractor());
 
   public final List<FieldValueMapping> processSchema(final Object schema, SchemaRegistryEnum registryEnum) {
-    return schemaRegistryMap.get(registryEnum).processSchema(schema);
+    Schema rawschema;
+    if(schema instanceof AvroSchema) {
+      rawschema = ((AvroSchema)schema).rawSchema();
+    }else {
+      rawschema = (Schema) schema;
+    }
+    return schemaRegistryMap.get(registryEnum).processSchema(rawschema);
   }
 
   public final List<String> getSchemaNameList(final String schema, SchemaRegistryEnum registryEnum) {
