da45ea05bc223c79761630ac8d6b5328ec847113
AzureAD\JuanRamilDiaz <juan.ramil@sngular.com>
2023-11-24 13:29:28 +0100

    Checkstyle fixes.

--- src/main/java/com/sngular/kloadgen/config/schemaregistry/DefaultPropertiesHelper.java
+++ src/main/java/com/sngular/kloadgen/config/schemaregistry/DefaultPropertiesHelper.java
@@ -21,7 +21,8 @@ class DefaultPropertiesHelper {
       PropertyMapping.builder().propertyName(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_AUTH_KEY).propertyValue(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_AUTH_BASIC_TYPE).build(),
       PropertyMapping.builder().propertyName(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_USERNAME_KEY).propertyValue(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_USERNAME_DEFAULT).build(),
       PropertyMapping.builder().propertyName(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_PASSWORD_KEY).propertyValue(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_PASSWORD_DEFAULT).build(),
-      PropertyMapping.builder().propertyName(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_AUTH_BEARER_KEY).propertyValue(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_AUTH_BEARER_DEFAULT).build(),
+      PropertyMapping.builder().propertyName(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_AUTH_BEARER_KEY).propertyValue(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_AUTH_BEARER_DEFAULT)
+                     .build(),
       PropertyMapping.builder().propertyName(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_SSL_KEYSTORE_LOCATION_KEY).propertyValue("").build(),
       PropertyMapping.builder().propertyName(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_SSL_KEYSTORE_PASSWORD_KEY).propertyValue("").build(),
       PropertyMapping.builder().propertyName(SchemaRegistryKeyHelper.SCHEMA_REGISTRY_SSL_TRUSTSTORE_LOCATION_KEY).propertyValue("").build(),
