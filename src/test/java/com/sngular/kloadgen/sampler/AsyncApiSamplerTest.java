55bd5ca50d5f71690fed69ae0eb3f9dc1e92fa06
Graciela Méndez Olmos <graciela.mendez@sngular.com>
2023-11-27 09:14:10 +0100

    Test AsyncApiSampler

--- src/test/java/com/sngular/kloadgen/sampler/AsyncApiSamplerTest.java
+++ src/test/java/com/sngular/kloadgen/sampler/AsyncApiSamplerTest.java
@@ -1,4 +1,85 @@
+package com.sngular.kloadgen.sampler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.sngular.kloadgen.extractor.ApiExtractor;
+import com.sngular.kloadgen.extractor.model.AsyncApiFile;
+import com.sngular.kloadgen.processor.fixture.JsonSchemaFixturesConstants;
+import com.sngular.kloadgen.testutil.FileHelper;
+import com.sngular.kloadgen.testutil.SchemaParseUtil;
+import com.sngular.kloadgen.util.ProducerKeysHelper;
+import com.sngular.kloadgen.util.PropsKeysHelper;
+import io.confluent.kafka.schemaregistry.ParsedSchema;
+import org.apache.jmeter.threads.JMeterContext;
+import org.apache.jmeter.threads.JMeterContextService;
+import org.apache.jmeter.threads.JMeterVariables;
+import org.apache.jmeter.util.JMeterUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class AsyncApiSamplerTest {
-  
+    private final FileHelper fileHelper = new FileHelper();
+
+    private JMeterContext jmcx;
+
+    private static Stream<Object> parametersForConfigureValueGeneratorTest() {
+        return Stream.of("localhost:8081", "");
+    }
+
+    private AsyncApiSampler sampler = new AsyncApiSampler();
+
+    private ApiExtractor apiExtractor;
+    @BeforeEach
+    public final void setUp() throws IOException {
+        final File file = new File("src/test/resources");
+        final String absolutePath = file.getAbsolutePath();
+        JMeterUtils.loadJMeterProperties(absolutePath + "/kloadgen.properties");
+        jmcx = JMeterContextService.getContext();
+        jmcx.setVariables(new JMeterVariables());
+        JMeterUtils.setLocale(Locale.ENGLISH);
+        sampler = new AsyncApiSampler();
+        apiExtractor = mock(ApiExtractor.class);
+
+    }
+
+    /*@Test
+    void testAsyncApiSampleBasic() throws IOException {
+        JsonNode asyncApiFileNode = mock(JsonNode.class);
+        sampler.setAsyncApiFileNode(asyncApiFileNode);
+        JMeterVariables jmvar = new JMeterVariables();
+        jmvar.putAll(getVariablesJsonSchema());
+
+
+
+
+    }*/
+
+
+    public JMeterVariables getVariablesJsonSchema() throws IOException {
+
+        final File testFile = fileHelper.getFile("/asyncapi/event-api.yml");
+        final ParsedSchema parsedSchema = SchemaParseUtil.getParsedSchema(testFile, "JSON");
+
+        final var variables = new JMeterVariables();
+        variables.put(PropsKeysHelper.KEY_SCHEMA_TYPE, "JSON");
+        variables.put(PropsKeysHelper.VALUE_SUBJECT_NAME, "jsonSubject");
+        variables.put(PropsKeysHelper.KEY_SUBJECT_NAME, "jsonSubject");
+        variables.put(PropsKeysHelper.VALUE_SCHEMA, String.valueOf(parsedSchema));
+        variables.put(PropsKeysHelper.KEY_SCHEMA, String.valueOf(parsedSchema));
+        variables.putObject(PropsKeysHelper.VALUE_SCHEMA_PROPERTIES, JsonSchemaFixturesConstants.SIMPLE_SCHEMA_NONREQUIRED);
+        variables.put(ProducerKeysHelper.KEY_NAME_STRATEGY, "theStrategy");
+        return variables;
+    }
+
+
+
 }
